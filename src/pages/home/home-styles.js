358fb6ee8e8fcef65ed248fabbd05d9fb4a4108d
rick427 <rchard@yahoo.com>
2020-09-14 11:28:40 +0100

    more adjustments

--- src/pages/home/home-styles.js
+++ src/pages/home/home-styles.js
@@ -81,7 +81,7 @@ export const StyledSection = styled.section`
         top: 50%;
         left: 50%;
         transform: translate(-50%, -50%);
-        width: 70%;
+        width: 60%;
         text-align: center;
         margin: 0 auto;
         letter-spacing: 1px;
